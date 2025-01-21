b7af0283a9c7422d3449c00c538f682ef0d41149
Ivan Dereviankin <ukrustacean@gmail.com>
2025-01-21 03:58:19 +0100

    Update the year

--- kito.cpp
+++ kito.cpp
@@ -1,4 +1,4 @@
-//       Copyright Ivan "ukrustacean" Dereviankin 2024.
+//       Copyright Ivan "ukrustacean" Dereviankin 2025.
 // Distributed under the Boost Software License, Version 1.0.
 //        (See accompanying file LICENSE or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
