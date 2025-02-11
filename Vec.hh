742d06fd566e8f94a675671f0410d2fcf64ddd08
Ivan "ukrustacean" Dereviankin <ukrustacean@gmail.com>
2025-02-11 21:50:56 +0100

    Don't forget to add license

--- Vec.hh
+++ Vec.hh
@@ -1,3 +1,8 @@
+//       Copyright Ivan "ukrustacean" Dereviankin 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//        (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef VEC_H
 #define VEC_H
 
